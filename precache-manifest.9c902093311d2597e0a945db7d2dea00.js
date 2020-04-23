55d7db50a6c1667db285cadd1548d14b7e8bca34
miguel231997 <miguelgalindo1214@gmail.com>
2020-04-23 14:58:39 -0400

    Updates

---
+++ precache-manifest.9c902093311d2597e0a945db7d2dea00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9bef5dab6ec1326c485e8fd8a6095c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "26f5b55d5971436c4849",
+    "url": "/robofriends/static/css/main.8d8e49b7.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/robofriends/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26f5b55d5971436c4849",
+    "url": "/robofriends/static/js/main.20c1da59.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
